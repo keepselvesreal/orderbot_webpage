063ca093e810571a9c3f20dc154268a2e819f32f
keepselvesreal <keep_selfs_real@naver.com>
2023-11-21 12:34:32 +0900

    django 인식 못하던 문제 해결, 어떤 이유로 제외된 APP들 다시 INSTALLED APPS 경로에 추가, OrderItem 모델 추가 admin.py에 추가, 장고 셸로 모델 데이터 생성

--- venv/order/models.py
+++ venv/order/models.py
@@ -2,6 +2,7 @@ from django.db import models
 from django.utils import timezone
 from django.contrib.auth.models import User
 
+
 class Order(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     order_time = models.DateTimeField(default=timezone.now)
